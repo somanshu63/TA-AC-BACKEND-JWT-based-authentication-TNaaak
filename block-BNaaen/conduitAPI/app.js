3ca3256246b27a134ef519115119de7b4f3fef75
somanshu63 <somanshu63@gmail.com>
2022-11-24 13:38:31 +0530

    change url of databse connection

--- block-BNaaen/conduitAPI/app.js
+++ block-BNaaen/conduitAPI/app.js
@@ -16,7 +16,7 @@ var userRouter = require("./routes/user");
 
 //connect database
 mongoose.connect(
-  "mongodb://127.0.0.1/conductApi",
+  "mongodb://127.0.0.1:27017/conductApi",
   { useUnifiedTopology: true },
   (err) => {
     console.log(err ? err : "connected to database");
